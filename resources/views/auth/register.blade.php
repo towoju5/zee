4017031e2e8f2b61c05fd80ef43b2f1e92558127
EMMANUEL TOWOJU <towojuads@gmail.com>
2024-01-10 00:11:39 +0100

    bussinessName added to registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,6 +14,11 @@
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
+            <div>
+                <x-label for="name" value="{{ __('Business Name') }}" />
+                <x-input id="bussinessName" class="block mt-1 w-full" type="text" name="bussinessName" :value="old('bussinessName')" required autofocus autocomplete="bussinessName" />
+            </div>
+
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
