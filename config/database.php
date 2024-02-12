6e8ee9d0193320153d41b1dc5d1eaa97cdb0e733
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2024-02-12 09:48:45 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
             ]) : [],
         ],
-
+ 
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
